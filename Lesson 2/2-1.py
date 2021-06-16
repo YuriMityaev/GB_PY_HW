c3e9f7c6d43849dc7fef94932652d303f7b107d3
YuriMityaev <eksia@bk.ru>
2021-06-16 22:39:31 +1100

    Задание 2-1 исправлены ошибки формата

--- Lesson 2/2-1.py
+++ Lesson 2/2-1.py
@@ -1,3 +1,3 @@
-list = [1 , "str", 32.2 , True, None]
-for a,b in  enumerate (list, 1):
+my_list = [1, "str", 32.2, True, None]
+for a, b in enumerate(my_list, 1):
     print(f"{a} = {b} - {type(b)}")
