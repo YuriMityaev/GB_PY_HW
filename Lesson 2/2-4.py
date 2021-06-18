c683f87328c69a069fa0443e0a0343dc52977336
YuriMityaev <eksia@bk.ru>
2021-06-18 18:28:01 +1100

    Выполнены задания 2-2 => 2-5 частично сам, частично после разбора, все удалось понять. Решение задания 2-6 не понял реализацию, буду пытаться разбираться.

--- Lesson 2/2-4.py
+++ Lesson 2/2-4.py
@@ -1,3 +1,3 @@
-str = input("Введите слово - ")
-for a, b in enumerate(str, 1):
+word = input("Введите слово - ")
+for a, b in enumerate(word, 1):
     print(a, b) if len(b) <= 10 else print(a, (b[:10]))
