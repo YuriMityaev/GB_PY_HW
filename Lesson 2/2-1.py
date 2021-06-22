bcea1eb7167dc95f6c0602c5a5b4021d307531ee
YuriMityaev <85499627+YuriMityaev@users.noreply.github.com>
2021-06-23 00:57:47 +1100

    Revert "Задание 2-1 исправлены ошибки формата"

--- Lesson 2/2-1.py
+++ Lesson 2/2-1.py
@@ -1,3 +1,3 @@
-my_list = [1, "str", 32.2, True, None]
-for a, b in enumerate(my_list, 1):
+list = [1 , "str", 32.2 , True, None]
+for a,b in  enumerate (list, 1):
     print(f"{a} = {b} - {type(b)}")
--- Lesson 2/2-4.py
+++
@@ -1,3 +0,0 @@
-word = input("Введите слово - ")
-for a, b in enumerate(word, 1):
-    print(a, b) if len(b) <= 10 else print(a, (b[:10]))
